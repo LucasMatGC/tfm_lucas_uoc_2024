b9b7a10eda4b68ecced9d4949626e83100ae588d
DESKTOP-Kiwi <lucasmatias.goncal@gmail.com>
2024-09-17 18:47:50 +0200

    Initial commit with template project.

---
+++ RogueLike_Project/Source/RogueLike_Project/RogueLike_ProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RogueLike_ProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARogueLike_ProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARogueLike_ProjectGameMode();
+};
+
+
+
